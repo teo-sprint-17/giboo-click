199e4595513f20ea1e04eecd2953e72da3b3efec
khundi <albert5428@gmail.com>
2024-04-06 03:49:38 +0900

    chore: Add new files and configurations for Next.js project

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "기부클릭",
+  description: "기부클릭은 기부를 쉽게 할 수 있는 플랫폼입니다.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
