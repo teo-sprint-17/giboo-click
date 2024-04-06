fbe3da42dd5cf0602bc4ccb6546c76f7bf9462f4
khundi <albert5428@gmail.com>
2024-04-06 18:40:47 +0900

    feat: 바텀 네비게이션 바 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function App() {
         <Category isSelected={false} name={'이주민'} />
       </div>
       <Carousel />
-      <div className="mt-[56px] mx-auto bg-[#D9D9D9] w-[240px] px-[71px] py-[8px] rounded-[60px] text-center text-SUITE font-semibold text-[28px] leading-34.94 cursor-pointer">
+      <div className="mt-[56px] mb-[60px] mx-auto bg-[#D9D9D9] w-[240px] px-[71px] py-[8px] rounded-[60px] text-center text-SUITE font-semibold text-[28px] leading-34.94 cursor-pointer">
         기부하기
       </div>
     </>
