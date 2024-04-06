97eb0353b23fb95312f3cb9a5ffdcae4384e22a8
khundi <albert5428@gmail.com>
2024-04-06 16:49:53 +0900

    feat: 캐러셀 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,39 @@
-const Home = () => {
-  return <div>Home</div>;
-};
+'use client';
 
-export default Home;
+import Carousel from '@/components/Home/Carousel';
+
+export default function App() {
+  return (
+    <Carousel
+      items={[
+        {
+          id: 1,
+          title: '하키선수의 꿈을 키우는\n12살 태우',
+          donationFoundation: '사단법인 기릭',
+          remainDay: 3,
+          onClick: () => {
+            console.log('clicked1');
+          },
+        },
+        {
+          id: 2,
+          title: '하키선수의 꿈을 키우는\n12살 태우2',
+          donationFoundation: '사단법인 기릭',
+          remainDay: 4,
+          onClick: () => {
+            console.log('clicked2');
+          },
+        },
+        {
+          id: 3,
+          title: '하키선수의 꿈을 키우는\n12살 태우3',
+          donationFoundation: '사단법인 기릭',
+          remainDay: 4,
+          onClick: () => {
+            console.log('clicked3');
+          },
+        },
+      ]}
+    />
+  );
+}
