1184b71a28dcb596f4b6e788a5f327987c8619d7
khundi <albert5428@gmail.com>
2024-04-06 17:22:00 +0900

    fix: 폰트 미적용 오류 수정, 캐러셀 리팩토링

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,12 @@
-'use client';
-
 import Carousel from '@/components/Home/Carousel';
 
 export default function App() {
   return (
-    <Carousel
-      items={[
-        {
-          id: 1,
-          title: '하키선수의 꿈을 키우는\n12살 태우',
-          donationFoundation: '사단법인 기릭',
-          remainDay: 3,
-          onClick: () => {
-            console.log('clicked1');
-          },
-        },
-        {
-          id: 2,
-          title: '하키선수의 꿈을 키우는\n12살 태우2',
-          donationFoundation: '사단법인 기릭',
-          remainDay: 4,
-          onClick: () => {
-            console.log('clicked2');
-          },
-        },
-        {
-          id: 3,
-          title: '하키선수의 꿈을 키우는\n12살 태우3',
-          donationFoundation: '사단법인 기릭',
-          remainDay: 4,
-          onClick: () => {
-            console.log('clicked3');
-          },
-        },
-      ]}
-    />
+    <>
+      <div className="whitespace-pre-wrap font-['SUITE-Variable']">
+        {'기부는 기부클릭이 할게요.\n여러분은 따듯한 마음만 전해주세요.'}
+      </div>
+      <Carousel />
+    </>
   );
 }
--- src/components/Home/Carousel.tsx
+++ src/components/Home/Carousel.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import Image from 'next/image';
 import 'swiper/css';
 import 'swiper/css/effect-coverflow';
@@ -5,17 +7,37 @@ import 'swiper/css/pagination';
 import { EffectCoverflow, Pagination } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
-type Props = {
-  items: {
-    id: number;
-    title: string;
-    donationFoundation: string;
-    remainDay: number;
-    onClick?: () => void;
-  }[];
-};
+const Carousel = () => {
+  const items = [
+    {
+      id: 1,
+      title: '하키선수의 꿈을 키우는\n12살 태우',
+      donationFoundation: '사단법인 기릭',
+      remainDay: 3,
+      onClick: () => {
+        console.log('clicked1');
+      },
+    },
+    {
+      id: 2,
+      title: '하키선수의 꿈을 키우는\n12살 태우2',
+      donationFoundation: '사단법인 기릭',
+      remainDay: 4,
+      onClick: () => {
+        console.log('clicked2');
+      },
+    },
+    {
+      id: 3,
+      title: '하키선수의 꿈을 키우는\n12살 태우3',
+      donationFoundation: '사단법인 기릭',
+      remainDay: 4,
+      onClick: () => {
+        console.log('clicked3');
+      },
+    },
+  ];
 
-const Carousel = ({ items }: Props) => {
   return (
     <Swiper
       spaceBetween={-200}
@@ -38,10 +60,10 @@ const Carousel = ({ items }: Props) => {
           <div className="w-[292px] h-[400px] overflow-hidden	rounded-[10px]">
             <div className="absolute w-full h-[50%] bg-gradient-to-t from-transparent via-white-100 to-white" />
             <div className="absolute w-full py-[10px] px-[20px]">
-              <div className="font-['SUITE Variable'] font-extrabold text-[24px] leading-29.95 text-left whitespace-pre-wrap">
+              <div className="font-['SUITE-Variable'] font-extrabold text-[24px] leading-29.95 text-left whitespace-pre-wrap">
                 {title}
               </div>
-              <div className="flex justify-between font-['SUITE Variable'] font-semibold text-[16px]">
+              <div className="flex justify-between font-['SUITE-Variable'] font-semibold text-[16px]">
                 <div className="">{donationFoundation}</div>
                 {remainDay >= 0 && <div className="">{`D-${remainDay}`}</div>}
               </div>
@@ -53,8 +75,8 @@ const Carousel = ({ items }: Props) => {
                   <div className={`w-[${62}%] h-full absolute rounded-[20px] top-0 left-0 bg-[#FF3838]`} />
                 </div>
                 <div className="w-full flex justify-between">
-                  <div className="font-['SUITE Variable']">총 620명이 참여중입니다.</div>
-                  <div className="font-['SUITE Variable']">62%</div>
+                  <div className="font-['SUITE-Variable']">총 620명이 참여중입니다.</div>
+                  <div className="font-['SUITE-Variable']">62%</div>
                 </div>
               </div>
             </div>
