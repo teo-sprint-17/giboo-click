4d65ae8b4892c3f1cfc14f5a60d756651a20a2f0
khundi <albert5428@gmail.com>
2024-04-07 15:57:52 +0900

    feat: 메인페이지 UI 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,17 +8,21 @@ export default function App() {
     <>
       <IntroModal />
       <div>
-        <div className="whitespace-pre-wrap font-['SUITE-Variable'] mb-[50px]">
+        <div className="whitespace-pre-wrap mb-[50px]">
           기부는 <span className="font-semibold">기부클릭</span>
           이 할게요.
           <br />
           여러분은 <span className="font-semibold">따듯한 마음</span>만 전해주세요.
         </div>
-        <div className="flex gap-[16px]">
-          <Category isSelected={true} name={'아동|청소년'} />
-          <Category isSelected={false} name={'지구촌'} />
-          <Category isSelected={false} name={'실버세대'} />
-          <Category isSelected={false} name={'이주민'} />
+        <div className="overflow-y-scroll">
+          <div className="flex gap-[16px]">
+            <Category isSelected={true} name={'#아동|청소년'} />
+            <Category isSelected={false} name={'#지구촌'} />
+            <Category isSelected={false} name={'#실버세대'} />
+            <Category isSelected={false} name={'#이주민'} />
+            <Category isSelected={false} name={'#이주민'} />
+            <Category isSelected={false} name={'#이주민'} />
+          </div>
         </div>
         <Carousel />
         <DonationButton />
--- src/components/common/Category/Category.tsx
+++ src/components/common/Category/Category.tsx
@@ -6,8 +6,8 @@ type Props = {
 const Category = ({ isSelected, name }: Props) => {
   return (
     <div
-      className={`font-['SUITE-Variable'] text-center text-base font-normal leading-5 py-[6px] px-[10px] rounded-[30px] cursor-pointer mb-[38px]${
-        isSelected ? ' bg-[#565656] text-[#EBEBEB]' : ' bg-[#D7D7CE] text-[#000000]'
+      className={`whitespace-nowrap text-center text-base font-normal leading-5 py-[6px] px-[10px] rounded-[30px] cursor-pointer mb-[38px]${
+        isSelected ? ' bg-[#FF6A6A] text-[#FFFFFF]' : ' bg-[#FFFFFF] text-[#000000]'
       }`}
     >
       {name}
